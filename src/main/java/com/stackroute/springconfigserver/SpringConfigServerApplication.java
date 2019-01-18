3a6e8ac5e4477f25df5a5a7572e10f9883b0a359
pratima.verma <pratima.verma@boeing.com>
2019-01-18 12:57:29 +0530

    final commit

---
+++ src/main/java/com/stackroute/springconfigserver/SpringConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.stackroute.springconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerApplication.class, args);
+	}
+
+}
+
